e724dbaa92dc29e2a22852ae758097d0df0be822
antoniosarro <tech@antoniosarro.dev>
2024-12-18 16:08:37 +0100

    test

--- hosts/nixos/laptop/default.nix
+++ hosts/nixos/laptop/default.nix
@@ -11,79 +11,45 @@
   pkgs,
   ...
 }: {
-  specialisation."integrated-gpu".configuration = {
-    imports = lib.flatten [
-      # ============================
-      # Hardware
-      # ============================
-      ./hardware-configuration.nix
-      inputs.hardware.nixosModules.common-cpu-intel
-      inputs.hardware.nixosModules.common-gpu-intel
-      inputs.hardware.nixosModules.common-pc-ssd
-      inputs.hardware.nixosModules.common-gpu-nvidia-disable
+  imports = lib.flatten [
+    # ============================
+    # Hardware
+    # ============================
+    ./hardware-configuration.nix
+    inputs.hardware.nixosModules.common-cpu-intel
+    inputs.hardware.nixosModules.common-gpu-intel
+    inputs.hardware.nixosModules.common-pc-ssd
+    inputs.hardware.nixosModules.common-gpu-nvidia-disable
 
-      # ============================
-      # Disk Layout
-      # ============================
-      inputs.disko.nixosModules.disko
-      (lib.custom.relativeToRoot "hosts/common/disks/laptop.nix")
+    # ============================
+    # Disk Layout
+    # ============================
+    inputs.disko.nixosModules.disko
+    (lib.custom.relativeToRoot "hosts/common/disks/laptop.nix")
 
-      (map lib.custom.relativeToRoot [
-        # Required Configs
-        "hosts/common/core"
+    (map lib.custom.relativeToRoot [
+      # Required Configs
+      "hosts/common/core"
 
-        # Optional Configs
-        "hosts/common/optional/services/greetd.nix" # display manager
-        "hosts/common/optional/services/printing.nix" # CUPS
-        "hosts/common/optional/audio.nix" # pipewire and cli controls
-        "hosts/common/optional/gaming.nix" # steam, gamescope, gamemode, and related hardware
-        "hosts/common/optional/hyprland.nix" # window manager
-        "hosts/common/optional/nvtop.nix" # GPU monitor
-        "hosts/common/optional/obsidian.nix" # Notes
-        "hosts/common/optional/plymouth.nix" # Animated boot screen
-        "hosts/common/optional/scanning.nix" # SANE and simple-scan
-        "hosts/common/optional/thunar.nix" # file manager
-        "hosts/common/optional/vlc.nix" # media player
-        "hosts/common/optional/wayland.nix" # wayland components and pkgs not available in home-manager
-      ])
-    ];
-  };
+      # Optional Configs
+      "hosts/common/optional/services/greetd.nix" # display manager
+      "hosts/common/optional/services/printing.nix" # CUPS
+      "hosts/common/optional/audio.nix" # pipewire and cli controls
+      "hosts/common/optional/gaming.nix" # steam, gamescope, gamemode, and related hardware
+      "hosts/common/optional/hyprland.nix" # window manager
+      "hosts/common/optional/nvtop.nix" # GPU monitor
+      "hosts/common/optional/obsidian.nix" # Notes
+      "hosts/common/optional/plymouth.nix" # Animated boot screen
+      "hosts/common/optional/scanning.nix" # SANE and simple-scan
+      "hosts/common/optional/thunar.nix" # file manager
+      "hosts/common/optional/vlc.nix" # media player
+      "hosts/common/optional/wayland.nix" # wayland components and pkgs not available in home-manager
+    ])
+  ];
 
   specialisation."dedicated-gpu".configuration = {
     imports = lib.flatten [
-      # ============================
-      # Hardware
-      # ============================
-      ./hardware-configuration.nix
-      inputs.hardware.nixosModules.common-cpu-intel
-      inputs.hardware.nixosModules.common-gpu-intel
-      inputs.hardware.nixosModules.common-pc-ssd
       inputs.hardware.nixosModules.common-gpu-nvidia
-
-      # ============================
-      # Disk Layout
-      # ============================
-      inputs.disko.nixosModules.disko
-      (lib.custom.relativeToRoot "hosts/common/disks/laptop.nix")
-
-      (map lib.custom.relativeToRoot [
-        # Required Configs
-        "hosts/common/core"
-
-        # Optional Configs
-        "hosts/common/optional/services/greetd.nix" # display manager
-        "hosts/common/optional/services/printing.nix" # CUPS
-        "hosts/common/optional/audio.nix" # pipewire and cli controls
-        "hosts/common/optional/gaming.nix" # steam, gamescope, gamemode, and related hardware
-        "hosts/common/optional/hyprland.nix" # window manager
-        "hosts/common/optional/nvtop.nix" # GPU monitor
-        "hosts/common/optional/obsidian.nix" # Notes
-        "hosts/common/optional/plymouth.nix" # Animated boot screen
-        "hosts/common/optional/scanning.nix" # SANE and simple-scan
-        "hosts/common/optional/thunar.nix" # file manager
-        "hosts/common/optional/vlc.nix" # media player
-        "hosts/common/optional/wayland.nix" # wayland components and pkgs not available in home-manager
-      ])
     ];
     hardware.nvidia.open = true;
     hardware.nvidia.prime = {
