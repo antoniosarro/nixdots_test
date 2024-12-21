c0d8d63a6f88b4e52068a5d9d382442c93037256
antoniosarro <tech@antoniosarro.dev>
2024-12-21 17:52:32 +0100

    test

--- home/ohhbigg/common/optional/desktops/waybar.nix
+++ home/ohhbigg/common/optional/desktops/waybar.nix
@@ -32,12 +32,12 @@
           [
             "tray"
           ]
-          ++ lib.optional (config.hostSpec.hostName == "laptop") ["backlight"]
+          ++ lib.optional (config.hostSpec.hostName == "laptop") "backlight"
           ++ [
             "pulseaudio"
             "network"
           ]
-          ++ lib.optional (config.hostSpec.hostName == "laptop") ["battery"]
+          ++ lib.optional (config.hostSpec.hostName == "laptop") "battery"
           ++ ["clock" "custom/power-menu"];
 
         # ============================
