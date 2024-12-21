94f635a53464b279d7523484afc8a5f137a87d0f
antoniosarro <tech@antoniosarro.dev>
2024-12-21 12:12:25 +0100

    test

--- home/ohhbigg/common/optional/desktops/hyprland/hyprpaper.nix
+++ home/ohhbigg/common/optional/desktops/hyprland/hyprpaper.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       preload = ["${config.home.homeDirectory}/media/images/wallpapers/1.jpeg"];
-      wallpaper = ["${config.home.homeDirectory}/media/images/wallpapers/1.jpeg"];
+      wallpaper = [",${config.home.homeDirectory}/media/images/wallpapers/1.jpeg"];
     };
   };
 }
