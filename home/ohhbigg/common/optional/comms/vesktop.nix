7aa15a48e15d99358e132b7e8190c2edf630d4bb
antoniosarro <tech@antoniosarro.dev>
2024-12-24 15:37:19 +0100

    test

--- home/ohhbigg/common/optional/comms/vesktop.nix
+++ home/ohhbigg/common/optional/comms/vesktop.nix
@@ -14,22 +14,22 @@
     force = true;
     text = ''
       :root {
-         --base00: ${config.hostSpec.theme.colors.base00};
-         --base01: ${config.hostSpec.theme.colors.base01};
-         --base02: ${config.hostSpec.theme.colors.base02};
-         --base03: ${config.hostSpec.theme.colors.base03};
-         --base04: ${config.hostSpec.theme.colors.base04};
-         --base05: ${config.hostSpec.theme.colors.base05};
-         --base06: ${config.hostSpec.theme.colors.base06};
-         --base07: ${config.hostSpec.theme.colors.base07};
-         --base08: ${config.hostSpec.theme.colors.base08};
-         --base09: ${config.hostSpec.theme.colors.base09};
-         --base0A: ${config.hostSpec.theme.colors.base0A};
-         --base0B: ${config.hostSpec.theme.colors.base0B};
-         --base0C: ${config.hostSpec.theme.colors.base0C};
-         --base0D: ${config.hostSpec.theme.colors.base0D};
-         --base0E: ${config.hostSpec.theme.colors.base0E};
-         --base0F: ${config.hostSpec.theme.colors.base0F};
+         --base00: #${config.hostSpec.theme.colors.base00};
+         --base01: #${config.hostSpec.theme.colors.base01};
+         --base02: #${config.hostSpec.theme.colors.base02};
+         --base03: #${config.hostSpec.theme.colors.base03};
+         --base04: #${config.hostSpec.theme.colors.base04};
+         --base05: #${config.hostSpec.theme.colors.base05};
+         --base06: #${config.hostSpec.theme.colors.base06};
+         --base07: #${config.hostSpec.theme.colors.base07};
+         --base08: #${config.hostSpec.theme.colors.base08};
+         --base09: #${config.hostSpec.theme.colors.base09};
+         --base0A: #${config.hostSpec.theme.colors.base0A};
+         --base0B: #${config.hostSpec.theme.colors.base0B};
+         --base0C: #${config.hostSpec.theme.colors.base0C};
+         --base0D: #${config.hostSpec.theme.colors.base0D};
+         --base0E: #${config.hostSpec.theme.colors.base0E};
+         --base0F: #${config.hostSpec.theme.colors.base0F};
 
          --primary-630: var(--base00); /* Autocomplete background */
          --primary-660: var(--base00); /* Search input background */
