9eb86f46c1589784ba6ced9e767f9866ca9eb12f
antoniosarro <tech@antoniosarro.dev>
2024-12-24 21:59:22 +0100

    test

--- home/ohhbigg/common/core/nixvim/plugins/default.nix
+++ home/ohhbigg/common/core/nixvim/plugins/default.nix
@@ -7,5 +7,6 @@
     # ============================
     nixvim-config.colorschemes.enable = lib.mkDefault true;
     nixvim-config.ui.colorizer.enable = lib.mkDefault true;
+    nixvim-config.ui.alpha.enable = lib.mkDefault true;
   };
 }
